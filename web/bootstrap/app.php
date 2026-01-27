6a381dee9905a2eef47bd97ee19234ac29e16ab5
Nurlan270 <nurlanismzade@gmail.com>
2026-01-27 19:16:12 +0400

    global validation exception handler added

--- web/bootstrap/app.php
+++ web/bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Devrabiul\ToastMagic\Facades\ToastMagic;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 use Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter;
 use Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect;
 
@@ -19,5 +21,14 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        //  Show toast for validation errors
+        $exceptions->render(function (ValidationException $e) {
+            if (request()->expectsJson()) {
+                return null;
+            }
+
+            foreach ($e->errors() as $msg) {
+                ToastMagic::error(__('toast.title.error.validation'), $msg[0]);
+            }
+        });
     })->create();
