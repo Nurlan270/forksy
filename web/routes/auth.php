dd9def214fb8a81b9e46686e32f04176c6deda7c
Nurlan270 <nurlanismzade@gmail.com>
2026-01-30 15:26:43 +0400

    auth: logout route added

--- web/routes/auth.php
+++ web/routes/auth.php
@@ -7,7 +7,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::prefix(LaravelLocalization::setLocale() . '/auth')
     ->middleware(['guest', 'localeCookieRedirect', 'localizationRedirect'])
-    ->name("auth.")->group(function () {
+    ->name('auth.')->group(function () {
     // Registration Routes
     Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
@@ -16,3 +16,9 @@ Route::prefix(LaravelLocalization::setLocale() . '/auth')
     Route::get('/login', [LoginController::class, 'show'])->name('login.show');
     Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 });
+
+// Logout Route
+Route::prefix(LaravelLocalization::setLocale() . '/auth')
+    ->middleware(['auth', 'localeCookieRedirect', 'localizationRedirect'])
+    ->post('/logout', [LoginController::class, 'logout'])
+    ->name('auth.logout');
