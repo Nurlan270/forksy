2aebfed6545a66001fa6c7a8cf8e5565677f0936
Nurlan270 <nurlanismzade@gmail.com>
2026-01-30 20:13:02 +0400

    role, avatar, banner fields added

--- web/database/migrations/0001_01_01_000000_create_users_table.php
+++ web/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('role')->default('user');
+            $table->string('avatar')->nullable();
+            $table->string('banner')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
 
