9b272d77746689b581c0db0abf1a5792805e69f9
Nurlan270 <nurlanismzade@gmail.com>
2026-01-30 15:26:28 +0400

    auth: login & logout logic added

--- web/app/Http/Controllers/Auth/LoginController.php
+++ web/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Devrabiul\ToastMagic\Facades\ToastMagic;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -12,8 +14,44 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function store()
+    public function store(LoginRequest $request)
     {
+        $validated = $request->validated();
 
+        if (auth()->attempt($validated, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+
+            ToastMagic::success(
+                __('toast.title.success.login'),
+                __('toast.message.success.login')
+            );
+
+            return redirect()->intended(
+                localizeRoute('welcome')
+            );
+        }
+
+        ToastMagic::error(
+            __('toast.title.error.validation'),
+            __('toast.message.error.login')
+        );
+
+        return back();
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        ToastMagic::info(
+            __('toast.title.success.logout')
+        );
+
+        return redirect()->intended(
+            localizeRoute('welcome')
+        );
     }
 }
