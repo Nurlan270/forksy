e98b5f6d02bcc8277958a31f9b7e730a387257de
Nurlan270 <nurlanismzade@gmail.com>
2026-02-03 22:02:43 +0400

    settings routes added

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\SettingsController;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
@@ -8,8 +9,17 @@ require_once __DIR__ . "/auth.php";
 Route::prefix(LaravelLocalization::setLocale())
     ->middleware(['localeCookieRedirect', 'localizationRedirect'])
     ->group(function () {
+    // Welcome Page Route
     Route::view('/', 'welcome')->name('welcome');
+});
 
-    Route::view('/settings', 'user.settings')
-        ->middleware('auth')->name('user.settings');
+Route::prefix(LaravelLocalization::setLocale())
+    ->middleware(['auth', 'localeCookieRedirect', 'localizationRedirect'])
+    ->name('user.')->group(function () {
+    // Settings Routes
+    Route::get('/settings', [SettingsController::class, 'show'])->name('settings');
+    Route::patch('/settings/profile', [SettingsController::class, 'updateProfile'])
+        ->name('settings.update.profile');
+    Route::patch('/settings/password', [SettingsController::class, 'updatePassword'])
+        ->name('settings.update.password');
 });
