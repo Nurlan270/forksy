e5f38e57a1a8c37aef84e0d8aab395a699c4663b
Nurlan270 <nurlanismzade@gmail.com>
2026-02-03 21:11:59 +0400

    filepond package & its css and plugin assets imported

---
+++ web/resources/js/filepond.js
@@ -0,0 +1,13 @@
+import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType
+);
+
+window.FilePond = FilePond;
