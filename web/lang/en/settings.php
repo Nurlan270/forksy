95f35fca0fab6ca111d274658e27bf8e0689545a
Nurlan270 <nurlanismzade@gmail.com>
2026-02-03 17:41:42 +0400

    localization: new translations added

---
+++ web/lang/en/settings.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'title' => 'Settings',
+    'subtitle' => 'Manage your account preferences and security.',
+
+    'profile' => [
+        'title' => 'Profile information',
+    ],
+
+    'password' => [
+        'title' => 'Change password',
+    ],
+
+    'fields' => [
+        'name' => 'Name',
+        'email' => 'Email address',
+        'avatar' => 'Avatar',
+        'banner' => 'Profile banner',
+        'current_password' => 'Current password',
+        'new_password' => 'New password',
+        'password_confirmation' => 'Confirm new password',
+    ],
+
+    'actions' => [
+        'save' => 'Save changes',
+        'update_password' => 'Update password',
+        'change_avatar' => 'Change avatar',
+        'change_banner' => 'Change banner',
+    ],
+
+    'danger' => [
+        'title' => 'Danger zone',
+        'subtitle' => 'Deleting your account is permanent and cannot be undone. Please be certain.',
+        'delete_account' => 'Delete account',
+    ],
+];
