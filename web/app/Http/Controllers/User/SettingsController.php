ed854fd77f4f6362b20d6e8adf0f999574a18d85
Nurlan270 <nurlanismzade@gmail.com>
2026-02-03 22:03:15 +0400

    settings controller added

---
+++ web/app/Http/Controllers/User/SettingsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Settings\UpdateProfileRequest;
+use App\Services\SettingsService;
+
+class SettingsController extends Controller
+{
+    public function __construct(
+        protected SettingsService $service
+    ) {}
+
+    public function show()
+    {
+        return view('user.settings');
+    }
+
+    public function updateProfile(UpdateProfileRequest $request)
+    {
+        return $this->service->updateProfile($request);
+    }
+
+    public function updatePassword()
+    {
+        //
+    }
+}
