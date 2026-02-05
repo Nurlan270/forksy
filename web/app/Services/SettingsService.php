cf43c0c0f936d759bd650e00737f808afca935f3
Nurlan270 <nurlanismzade@gmail.com>
2026-02-05 17:53:31 +0400

    add avatar and banner upload functionality in profile update

--- web/app/Services/SettingsService.php
+++ web/app/Services/SettingsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Enums\StorageDisk;
 use App\Http\Requests\Settings\UpdateProfileRequest;
 use Devrabiul\ToastMagic\Facades\ToastMagic;
 use Illuminate\Http\RedirectResponse;
@@ -10,9 +11,18 @@ class SettingsService
 {
     public function updateProfile(UpdateProfileRequest $request): RedirectResponse
     {
-//        dd($request->all());
-
         $validated = $request->validated();
+
+        $newAvatar = updateOrCreateFile(
+            StorageDisk::Avatars, $request->file('avatar'), auth()->user()->avatar
+        );
+
+        $newBanner = updateOrCreateFile(
+            StorageDisk::Banners, $request->file('banner'), auth()->user()->banner
+        );
+
+        if ($newAvatar) $validated['avatar'] = $newAvatar;
+        if ($newBanner) $validated['banner'] = $newBanner;
 
         auth()->user()->update($validated);
 
