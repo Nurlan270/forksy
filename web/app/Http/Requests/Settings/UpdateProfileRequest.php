7dbfb1442af5d9850fc15209dad10fca1243d923
Nurlan270 <nurlanismzade@gmail.com>
2026-02-05 17:53:57 +0400

    add validation rules for banner and avatar image uploads in profile update

--- web/app/Http/Requests/Settings/UpdateProfileRequest.php
+++ web/app/Http/Requests/Settings/UpdateProfileRequest.php
@@ -15,10 +15,26 @@ class UpdateProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'banner'   => [
+                'sometimes', 'image', 'mimes:jpeg,png,jpg', 'max:5000',
+                Rule::dimensions()
+                    ->minWidth(1200)
+                    ->minHeight(300)
+                    ->maxWidth(3000)
+                    ->maxHeight(800),
+            ],
+            'avatar'   => [
+                'sometimes', 'image', 'mimes:jpeg,png,jpg', 'max:5000',
+                Rule::dimensions()
+                    ->minWidth(200)
+                    ->minHeight(200)
+                    ->maxWidth(1000)
+                    ->maxHeight(1000),
+            ],
             'name'     => ['sometimes', 'string', 'min:2', 'max:50', 'regex:/^[A-Za-zА-Яа-яЁё ]+$/u'],
             'username' => [
                 'sometimes', 'string', 'min:2', 'max:70', 'regex:/^[A-Za-z0-9._-]+$/',
-                Rule::unique('users')->ignore(auth()->user())
+                Rule::unique('users')->ignore(auth()->user()),
             ],
         ];
     }
