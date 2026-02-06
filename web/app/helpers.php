03e393a1b9ac86f42e082bafc82814672da580d4
Nurlan270 <nurlanismzade@gmail.com>
2026-02-06 19:32:51 +0400

    new helper functions added

--- web/app/helpers.php
+++ web/app/helpers.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Enums\StorageDisk;
+use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 if (! function_exists("localizeRoute")) {
@@ -25,22 +27,44 @@ if (! function_exists("updateOrCreateFile")) {
     /**
      * Update a file on the specified disk or create it if it doesn't exist.
      *
-     * @param  StorageDisk  $disk
-     * @param  ?UploadedFile  $file
-     * @param  ?string  $oldPath
-     * @return string|false The path of the newly stored file. (false if no file was provided)
+     * @param UploadedFile|File|null $file The file to be stored. If null, no action will be taken.
+     * @param StorageDisk|string $disk The storage disk where the file should be stored.
+     * @param string|null $oldFilename The filename of the old file to be deleted.
+     * @return string|false The filename of the newly stored file. (false if no file was provided)
      */
-    function updateOrCreateFile(StorageDisk $disk, ?UploadedFile $file, ?string $oldPath = null): string|false
+    function updateOrCreateFile(
+        UploadedFile|File|null $file,
+        StorageDisk|string $disk = 'public',
+        ?string $oldFilename = null
+    ): string|false
     {
         if (!$file) return false;
 
         $storage = Storage::disk($disk);
-        $oldPath = Str::after($oldPath, 'storage/'. $disk->value . '/');
 
-        if ($oldPath && $storage->exists($oldPath)) {
-            $storage->delete($oldPath);
+        if ($oldFilename && $storage->exists($oldFilename)) {
+            $storage->delete($oldFilename);
         }
 
-        return $storage->putFile('', $file);
+        return $storage->putFile('/', $file);
+    }
+}
+
+if (! function_exists("generateDefaultAvatar")) {
+    /**
+     * Generate an avatar based on the user's name.
+     *
+     * @param string $name The name of the user for whom the avatar is being generated.
+     * @return string Avatar's filename.
+     */
+    function generateDefaultAvatar(string $name): string
+    {
+        $filename = 'default-' . Str::uuid() . '.png';
+
+        app('avatar')->create($name)->save(
+            storage_path('app/public/avatars/' . $filename)
+        );
+
+        return $filename;
     }
 }
