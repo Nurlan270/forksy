d6918ad405269de850ac26aa51827bb882b2d0dd
Nurlan270 <nurlanismzade@gmail.com>
2026-02-06 19:30:06 +0400

    add avatar and banner images upload

--- web/app/Services/SettingsService.php
+++ web/app/Services/SettingsService.php
@@ -6,29 +6,45 @@ use App\Enums\StorageDisk;
 use App\Http\Requests\Settings\UpdateProfileRequest;
 use Devrabiul\ToastMagic\Facades\ToastMagic;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class SettingsService
 {
     public function updateProfile(UpdateProfileRequest $request): RedirectResponse
     {
         $validated = $request->validated();
+        $user = auth()->user();
 
+        // Handle avatar and banner updates.
+        // If the user uploaded a new file, it will replace the old one.
         $newAvatar = updateOrCreateFile(
-            StorageDisk::Avatars, $request->file('avatar'), auth()->user()->avatar
-        );
+            $request->file('avatar'), StorageDisk::Avatars, $user->avatar);
 
         $newBanner = updateOrCreateFile(
-            StorageDisk::Banners, $request->file('banner'), auth()->user()->banner
-        );
+            $request->file('banner'), StorageDisk::Banners, $user->banner);
 
+        // If the user is changing their name, and they don't have custom avatar,
+        // delete the old default avatar and generate a new one based on the new name.
+        if (!$newAvatar &&
+            $user->name !== $validated['name'] &&
+            Str::contains($user->avatar, 'default-')
+        ) {
+            if (Storage::disk('avatars')->exists($user->avatar)) {
+                Storage::disk('avatars')->delete($user->avatar);
+            }
+
+            $newAvatar = generateDefaultAvatar($validated['name']);
+        }
+
+        // Update the avatar and banner paths if new files were uploaded or generated.
         if ($newAvatar) $validated['avatar'] = $newAvatar;
         if ($newBanner) $validated['banner'] = $newBanner;
 
-        auth()->user()->update($validated);
+        $user->update($validated);
 
         ToastMagic::success(
-            __('toast.title.success.settings.profile')
-        );
+            __('toast.title.success.settings.profile'));
 
         return back();
     }
